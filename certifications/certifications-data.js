a3b3ac26bdb621b894fe0f24099673f2313e92da
Isaac Muñoz <Ginkue@proton.me>
2026-07-29 22:42:41 +0000

    Nueva certificación vía n8n

--- certifications/certifications-data.js
+++ certifications/certifications-data.js
@@ -1,75 +1,68 @@
-/**
- * certifications/certifications-data.js
- * Fuente de datos para la página de certificaciones.
- * Cada objeto = una certificación.
- *
- * Campos:
- *   id     -> identificador único (slug)
- *   name   -> nombre de la certificación.
- *   issuer -> quién la emitió (Cisco, AWS, etc).
- *   image  -> logo/badge (opcional, vacío = ícono genérico).
- *   url    -> link público de verificación (Credly u otro).
- *   date   -> fecha de obtención, formato "YYYY-MM" (opcional).
- */
 const CERTIFICATIONS_DATA = {
-
-  certifications: [
+  "certifications": [
     {
-      id: "computer-hardware-basics",
-      name: "Computer Hardware Basics",
-      issuer: "Cisco Networking Academy",
-      image: "https://images.credly.com/size/220x220/images/19e742ef-13be-4d26-87ed-ac8f5fd0643c/image.png",
-      url: "https://www.credly.com/badges/794a54fc-4342-4cd8-a067-36c04bba2b16/public_url",
-      date: "2025-12",
+      "id": "computer-hardware-basics",
+      "name": "Computer Hardware Basics",
+      "issuer": "Cisco Networking Academy",
+      "image": "https://images.credly.com/size/220x220/images/19e742ef-13be-4d26-87ed-ac8f5fd0643c/image.png",
+      "url": "https://www.credly.com/badges/794a54fc-4342-4cd8-a067-36c04bba2b16/public_url",
+      "date": "2025-12"
     },
     {
-      id: "linux-essentials",
-      name: "Linux Essentials",
-      issuer: "Network Development Group",
-      image: "https://images.credly.com/size/220x220/images/e8fe3d67-2967-43d0-bc4a-7a268a37f47b/image.png",
-      url: "https://www.credly.com/badges/b5448c50-5885-4ff6-88b4-1152f5d412ab/public_url",
-      date: "2025-11",
+      "id": "linux-essentials",
+      "name": "Linux Essentials",
+      "issuer": "Network Development Group",
+      "image": "https://images.credly.com/size/220x220/images/e8fe3d67-2967-43d0-bc4a-7a268a37f47b/image.png",
+      "url": "https://www.credly.com/badges/b5448c50-5885-4ff6-88b4-1152f5d412ab/public_url",
+      "date": "2025-11"
     },
     {
-      id: "networking-basics",
-      name: "Networking Basics",
-      issuer: "Cisco Networking Academy",
-      image: "https://images.credly.com/size/220x220/images/5bdd6a39-3e03-4444-9510-ecff80c9ce79/image.png",
-      url: "https://www.credly.com/badges/80002e0e-2d30-4047-a55f-f3546eb441f3/public_url",
-      date: "2025-11",
+      "id": "networking-basics",
+      "name": "Networking Basics",
+      "issuer": "Cisco Networking Academy",
+      "image": "https://images.credly.com/size/220x220/images/5bdd6a39-3e03-4444-9510-ecff80c9ce79/image.png",
+      "url": "https://www.credly.com/badges/80002e0e-2d30-4047-a55f-f3546eb441f3/public_url",
+      "date": "2025-11"
     },
     {
-      id: "aws-foundations",
-      name: "AWS Foundations",
-      issuer: "AWS Academy",
-      image: "https://images.credly.com/size/160x160/images/e3541a0c-dd4a-4820-8052-5001006efc85/blob",
-      url: "https://www.credly.com/badges/539fb1b9-c328-4ba1-a15c-b5a6dc3c826c/public_url",
-      date: "2026-07",
+      "id": "aws-foundations",
+      "name": "AWS Foundations",
+      "issuer": "AWS Academy",
+      "image": "https://images.credly.com/size/160x160/images/e3541a0c-dd4a-4820-8052-5001006efc85/blob",
+      "url": "https://www.credly.com/badges/539fb1b9-c328-4ba1-a15c-b5a6dc3c826c/public_url",
+      "date": "2026-07"
     },
     {
-      id: "cisco-capture-the-flag",
-      name: "Cisco CyberGames 2026 - Capture the Flag",
-      issuer: "Cisco Networking Academy",
-      image: "https://images.credly.com/size/220x220/images/f3534616-ba00-42b2-bdb6-8a8420e5405a/blob",
-      url: "https://www.credly.com/badges/acff0b52-c76d-4b16-9be9-c160d35cd323/public_url",
-      date: "2026-07",
+      "id": "cisco-capture-the-flag",
+      "name": "Cisco CyberGames 2026 - Capture the Flag",
+      "issuer": "Cisco Networking Academy",
+      "image": "https://images.credly.com/size/220x220/images/f3534616-ba00-42b2-bdb6-8a8420e5405a/blob",
+      "url": "https://www.credly.com/badges/acff0b52-c76d-4b16-9be9-c160d35cd323/public_url",
+      "date": "2026-07"
     },
     {
-      id: "ethical-hacker",
-      name: "Ethical Hacker",
-      issuer: "Cisco Networking Academy",
-      image: "https://images.credly.com/size/680x680/images/242902b5-f527-42ad-865e-977c9e1b5b58/image.png",
-      url: "https://www.credly.com/badges/4505b6b1-b8d9-42e8-86ac-42b2d91a1eed/public_url",
-      date: "2026-06",
+      "id": "ethical-hacker",
+      "name": "Ethical Hacker",
+      "issuer": "Cisco Networking Academy",
+      "image": "https://images.credly.com/size/680x680/images/242902b5-f527-42ad-865e-977c9e1b5b58/image.png",
+      "url": "https://www.credly.com/badges/4505b6b1-b8d9-42e8-86ac-42b2d91a1eed/public_url",
+      "date": "2026-06"
     },
     {
-      id: "ccna-intro-networks",
-      name: "CCNA: Introduction to Networks",
-      issuer: "Academia de Cisco de la UTP",
-      image: "https://images.credly.com/size/220x220/images/70d71df5-f3dc-4380-9b9d-f22513a70417/CCNAITN__1_.png",
-      url: "https://www.credly.com/badges/3a6bc368-90a5-4f99-bbb0-1ac28b8c1d04/public_url",
-      date: "2026-04",
+      "id": "ccna-intro-networks",
+      "name": "CCNA: Introduction to Networks",
+      "issuer": "Academia de Cisco de la UTP",
+      "image": "https://images.credly.com/size/220x220/images/70d71df5-f3dc-4380-9b9d-f22513a70417/CCNAITN__1_.png",
+      "url": "https://www.credly.com/badges/3a6bc368-90a5-4f99-bbb0-1ac28b8c1d04/public_url",
+      "date": "2026-04"
     },
-  ],
-
+    {
+      "id": "testname",
+      "name": "testName",
+      "issuer": "testIssuer",
+      "image": "testImage",
+      "url": "testUrl",
+      "date": "testDate"
+    }
+  ]
 };
