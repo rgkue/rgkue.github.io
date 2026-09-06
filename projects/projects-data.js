96ffb1d93ccbaaddb9f881cf241ff6176e4f8098
Isaac Muñoz <Ginkue@proton.me>
2026-09-06 12:49:42 -0500

    Nuevo proyecto vía n8n

--- projects/projects-data.js
+++ projects/projects-data.js
@@ -1,99 +1,129 @@
 // projects/data.js
 const portfolioData = {
-  projects: [
+  "projects": [
     {
-      category: "Projects",
-      icon: "🌐",
-      items: [
+      "category": "Projects",
+      "icon": "🌐",
+      "items": [
         {
-          title: "Servidor Ubuntu como Router Domestico",
-          description: "Configuración de Ubuntu Server como router/access point editando archivos del sistema, con monitoreo de tráfico en red local y resolución de DNS mediante scripts Bash.",
-          tags: ["Ubuntu", "Linux", "Networking", "Bash", "Router"],
-          status: "done",
-          repo: "https://github.com/rgkue/ubuntuserver-router",
+          "title": "Servidor Ubuntu como Router Domestico",
+          "description": "Configuración de Ubuntu Server como router/access point editando archivos del sistema, con monitoreo de tráfico en red local y resolución de DNS mediante scripts Bash.",
+          "tags": [
+            "Ubuntu",
+            "Linux",
+            "Networking",
+            "Bash",
+            "Router"
+          ],
+          "status": "done",
+          "repo": "https://github.com/rgkue/ubuntuserver-router"
         },
         {
-          title: "Sitio web personal - Portafolio Profesional",
-          description: "Desarrollo de este portafolio personal utilizando HTML, CSS y JavaScript para mostrar proyectos, habilidades y experiencia profesional de forma clara y atractiva",
-          tags: ["HTML", "CSS", "JavaScript", "Web Development"],
-          status: "done",
-          repo: "https://github.com/rgkue/rgkue.github.io",
+          "title": "Sitio web personal - Portafolio Profesional",
+          "description": "Desarrollo de este portafolio personal utilizando HTML, CSS y JavaScript para mostrar proyectos, habilidades y experiencia profesional de forma clara y atractiva",
+          "tags": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Web Development"
+          ],
+          "status": "done",
+          "repo": "https://github.com/rgkue/rgkue.github.io"
         },
         {
-          title: "Shell Colors",
-          description: "Pequeño programa en bash scripting para simular el uso de una shell o terminal.",
-          tags: ["Programming", "Linux", "Terminal", "Shell", "Scripting", "Bash"],
-          status: "in-progress",
-          repo: "https://github.com/rgkue/shell-colors",
+          "title": "Shell Colors",
+          "description": "Pequeño programa en bash scripting para simular el uso de una shell o terminal.",
+          "tags": [
+            "Programming",
+            "Linux",
+            "Terminal",
+            "Shell",
+            "Scripting",
+            "Bash"
+          ],
+          "status": "in-progress",
+          "repo": "https://github.com/rgkue/shell-colors"
         }
-      ],
+      ]
     },
     {
-      category: "Cybersecurity",
-      icon: "🔒",
-      items: [
+      "category": "Cybersecurity",
+      "icon": "🔒",
+      "items": [
         {
-          title: "Simulación de Ransomware en Windows 7 Home Basic",
-          description: "Laboratorio de ransomware en entorno controlado contra un sistema operativo Windows 7 Home Basic vulnerable a MS17-010, EternaBlue",
-          tags: ["Python", "Malware", "Educativo", "EternaBlue", "MS17-010", "Ransomware"],
-          status: "done",
-          repo: "https://github.com/rgkue/xcrypto-lab",
+          "title": "Simulación de Ransomware en Windows 7 Home Basic",
+          "description": "Laboratorio de ransomware en entorno controlado contra un sistema operativo Windows 7 Home Basic vulnerable a MS17-010, EternaBlue",
+          "tags": [
+            "Python",
+            "Malware",
+            "Educativo",
+            "EternaBlue",
+            "MS17-010",
+            "Ransomware"
+          ],
+          "status": "done",
+          "repo": "https://github.com/rgkue/xcrypto-lab"
         },
         {
-          title: "Writeups - Documentación de CTFs en plataformas de pentesting",
-          description: "Repositorio con writeups de máquinas resueltas en plataformas como HackTheBox, TryHackMe y DockerLabs.",
-          tags: ["CTF", "HTB", "TryHackMe", "DockerLabs", "Pentesting", "Writeups"],
-          status: "done",
-          repo: "https://github.com/rgkue/writeups-pentesting",
+          "title": "Writeups - Documentación de CTFs en plataformas de pentesting",
+          "description": "Repositorio con writeups de máquinas resueltas en plataformas como HackTheBox, TryHackMe y DockerLabs.",
+          "tags": [
+            "CTF",
+            "HTB",
+            "TryHackMe",
+            "DockerLabs",
+            "Pentesting",
+            "Writeups"
+          ],
+          "status": "done",
+          "repo": "https://github.com/rgkue/writeups-pentesting"
         },
         {
-          title: "BashCheck",
-          description: "Herramienta de línea de comandos para la rápida verificación de hashes en archivos y exportación masiva de activos digitales",
-          tags: ["Bash", "Programming", "BashScripting", "Linux"],
-          status: "done",
-          repo: "https://github.com/rgkue/bashcheck",
-        },
-      ],
-    },
-    {
-      category: "Development",
-      icon: "💻",
-      items: [
-        {
-          title: "Proyecto CRUD en Python con Flask y SQLite usando una API REST",
-          description: "Desarrollo de un servidor y un cliente dedicado con interfaz CLI/GUI para la consulta y gestión de datos en una base de datos SQLite mediante una API REST.",
-          tags: ["Python", "Automation", "Development", "Flask", "SQLite", "REST API"],
-          status: "done",
-          repo: "https://github.com/rgkue/rest-api",
-        },
-      ],
-    },
-    /*    {
-      category: "Networking",
-      icon: "🌐",
-      items: [
-        {
-          title: "Configuración de Redes Locales con Linux",
-          description: "Laboratorio para la configuración y administración de redes locales utilizando herramientas de Linux.",
-          tags: ["Linux", "Networking", "Network Administration"],
-          status: "in-progress",
-          repo: "https://github.com/rgkue/local-networking-lab",
+          "title": "BashCheck",
+          "description": "Herramienta de línea de comandos para la rápida verificación de hashes en archivos y exportación masiva de activos digitales",
+          "tags": [
+            "Bash",
+            "Programming",
+            "BashScripting",
+            "Linux"
+          ],
+          "status": "done",
+          "repo": "https://github.com/rgkue/bashcheck"
         }
-      ],
+      ]
     },
     {
-      category: "Harwdare Hacking",
-      icon: "⚡",
-      items: [
+      "category": "Development",
+      "icon": "💻",
+      "items": [
         {
-          title: "Análisis de Vulnerabilidades en Dispositivos IoT",
-          description: "Investigación y análisis de vulnerabilidades en dispositivos IoT comunes, con recomendaciones para mejorar la seguridad.",
-          tags: ["IoT", "Hardware Hacking", "Vulnerabilities"],
-          status: "planned",
-          repo: null,
+          "title": "Proyecto CRUD en Python con Flask y SQLite usando una API REST",
+          "description": "Desarrollo de un servidor y un cliente dedicado con interfaz CLI/GUI para la consulta y gestión de datos en una base de datos SQLite mediante una API REST.",
+          "tags": [
+            "Python",
+            "Automation",
+            "Development",
+            "Flask",
+            "SQLite",
+            "REST API"
+          ],
+          "status": "done",
+          "repo": "https://github.com/rgkue/rest-api"
         },
-      ],
-    },
-  */ 
+        {
+          "title": "Reyscribe",
+          "description": "Software empaquetado en ejecutable para la transcripción de audios y vídeos.",
+          "tags": [
+            "development",
+            "software-solution",
+            "python",
+            "gradio",
+            "transcription"
+          ],
+          "status": "done",
+          "repo": "https://github.com/rgkue/reyscribe/"
+        }
+      ]
+    }
   ]
 };
